4a3579fd561be45bfd3fc668f6b34e75da19872a
zt <3141616128@qq.com>
2022-03-27 10:40:17 +0800

    feat:login UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:ztian_music_app/api/hello.dart';
+import 'package:ztian_music_app/pages/home.dart';
+import 'package:ztian_music_app/pages/login.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ZtianMusic());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ZtianMusic extends StatelessWidget {
+  const ZtianMusic({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ZtianMusic',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -25,82 +26,10 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'ZTianMusic'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String message = "ztian music";
-  final ButtonStyle buttonStyle =
-      ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _fetchData() {
-    sayHello().then((value) => setState(() {
-          message = value;
-        }));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(message, style: Theme.of(context).textTheme.headline5),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              style: buttonStyle,
-              onPressed: _fetchData,
-              child: const Text('Enabled'),
-            ),
-            const SizedBox(height: 30),
-            const Text(
-              '点击次数为:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add_alert_sharp),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      routes: {
+        '/': (context) => const Home(),
+        '/login': (context) => const Login()
+      },
     );
   }
 }
