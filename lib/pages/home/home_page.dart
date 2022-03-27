fef8b89ad3bcef3e04192ddfce0f4a644663f4ae
zt <3141616128@qq.com>
2022-03-27 18:11:32 +0800

    feat:musician component

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ztian_music_app/pages/home/header.dart';
 
+import 'musician_banner.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -8,7 +10,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-      children: const <Widget>[Header()],
+      children: const <Widget>[
+        Header(),
+        SizedBox(
+          height: 40,
+        ),
+        MusicianBanner(
+          title: "热榜音乐人",
+        )
+      ],
     ));
   }
 }
